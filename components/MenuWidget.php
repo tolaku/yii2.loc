aa649cbd30875aa1638f2c20627e9bebdd4bb9d4
Anatol Kulesh <Anatol Kulesh>
2018-02-20 23:10:31 +0300

    3 урок

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -8,11 +8,15 @@ class MenuWidget extends Widget {
 	public $tpl;
 
 	public function init(){
-		
+		parent::init();
+		if( $this->tpl === null ) {
+			$this->tpl = 'menu';
+		}
+		$this->tpl .= '.php';
 	}
 
 	public function run(){
-		return 'MENU';
+		return $this->tpl;
 	}
 }
 
