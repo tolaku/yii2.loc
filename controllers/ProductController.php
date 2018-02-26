caf37e26579e4999c0f43a4473bcb2ab2a4d31e1
Anatol Kulesh <Anatol Kulesh>
2018-02-26 21:15:52 +0300

    10 урок

---
+++ controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php 
+namespace app\controllers;
+
+use app\models\Category;
+use app\models\Product;
+use Yii;
+
+class ProductController extends AppController{
+
+	public function actionView($id){
+		$id = Yii::$app->request->get('id');
+		$product = Product::findOne($id);
+		//$product = Product::find()->with('category')->where(['id' => $id])->limit(1)->one();
+		return $this->render('view', compact('product'));
+	}
+
+}
