17b354a764df088b50f78f7dc71e7c72367cb7d9
Anatol <tola_k@mail.ru>
2018-02-27 10:43:28 +0300

    13 урок

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -8,7 +8,7 @@ use Yii;
 class ProductController extends AppController{
 
 	public function actionView($id){
-		$id = Yii::$app->request->get('id');
+		//$id = Yii::$app->request->get('id');
 		$product = Product::findOne($id);
 
 		if(empty($product))
