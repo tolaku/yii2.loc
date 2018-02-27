47bca96a52940091fda0c7c0b83462d60bd8b507
Anatol <tola_k@mail.ru>
2018-02-27 09:20:49 +0300

    12 урок

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -16,10 +16,16 @@ class CategoryController extends AppController{
 
 	public function actionView(){
 		$id = Yii::$app->request->get('id');
+
+		$category = Category::findOne($id);
+
+		if(empty($category))
+			throw new \yii\web\HttpException(404, 'Такой категории нет.');
+
 		/*$products = Product::find()->where(['category_id' => $id])->all();*/
 		$query = Product::find()->where(['category_id' => $id]);
 		$pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 3, 'forcePageParam' => false, 'pageSizeParam' => false]);
-		$category = Category::findOne($id);
+		
 		$this->setMeta('E-SHOPPER | '. $category->name, $category->keywords, $category->description);
 		$products = $query->offset($pages->offset)->limit($pages->limit)->all();
 		return $this->render('view', compact('products', 'pages', 'category'));
