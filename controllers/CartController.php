c86d2aeb3c32b4ebb11eb70312e61f857c7e6488
Anatol Kulesh <Anatol Kulesh>
2018-03-02 21:21:32 +0300

    20 урок

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -58,9 +58,37 @@ class CartController extends AppController{
 		$this->setMeta('Корзина');
 		$order = new Order();
 		if( $order->load(Yii::$app->request->post()) ){
-			debug(Yii::$app->request->post());
+			$order->qty = $session['cart.qty'];
+			$order->sum = $session['cart.sum'];
+			if($order->save()){
+				$this->saveOrderItems($session['cart'], $order->id);
+				// если заказ совершен, выведем сообщение
+				Yii::$app->session->setFlash('success', 'Ваш заказ принят. Менеджер вскоре свяжется с Вами.');
+				// очищаем корзину
+				$session->remove('cart');
+				$session->remove('cart.qty');
+				$session->remove('cart.sum');
+
+				// в случае успеха, перезагружаем страницу
+				return $this->refresh();
+			}else{
+				Yii::$app->session->setFlash('error', 'Ошибка оформления заказа!');
+			}
 		}
 		return $this->render('view', compact('session', 'order'));
+	}
+
+	protected function saveOrderItems($items, $order_id){
+		foreach ($items as $id => $item) {
+			$order_items = new OrderItems();
+			$order_items->order_id = $order_id;
+			$order_items->product_id = $id;
+			$order_items->name = $item['name'];
+			$order_items->price = $item['price'];
+			$order_items->qty_item = $item['qty'];
+			$order_items->sum_item = $item['qty'] * $item['price'];
+			$order_items->save();
+		}
 	}
 
 }
--- models/Order.php
+++ models/Order.php
@@ -5,6 +5,7 @@ namespace app\models;
 use \yii\db\ActiveRecord;
 use Yii;
 use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
 
 /**
  * This is the model class for table "order".
@@ -28,6 +29,21 @@ class Order extends ActiveRecord
     public static function tableName()
     {
         return 'order';
+    }
+
+    // метод поведение
+    public function behaviors(){
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                // если вместо метки времени UNIX используется datetime:
+                'value' => new Expression('NOW()'),
+            ],
+        ];
     }
 
     public function getOrderItems(){
