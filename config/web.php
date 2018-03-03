0ba1de0f25d44b5e49919ecc29528213cca77c0d
Anatol Kulesh <Anatol Kulesh>
2018-03-03 12:08:06 +0300

    21 урок

--- config/web.php
+++ config/web.php
@@ -34,7 +34,15 @@ $config = [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+       /*     'useFileTransport' => true,
+             'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.mail.ru',
+                'username' => 'username',
+                'password' => 'password',
+                'port' => '465',
+                'encryption' => 'ssl',
+            ],*/
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
--- controllers/CartController.php
+++ controllers/CartController.php
@@ -64,6 +64,11 @@ class CartController extends AppController{
 				$this->saveOrderItems($session['cart'], $order->id);
 				// если заказ совершен, выведем сообщение
 				Yii::$app->session->setFlash('success', 'Ваш заказ принят. Менеджер вскоре свяжется с Вами.');
+				Yii::$app->mailer->compose('order', ['session' => $session])
+					->setFrom(['test@mail.ru' => 'Yii2.loc'])
+					->setTo($order->email)
+					->setSubject('Заказ')
+					->send();
 				// очищаем корзину
 				$session->remove('cart');
 				$session->remove('cart.qty');
